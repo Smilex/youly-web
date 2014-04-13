af5e997615cda650f1cefde72036317ed1ca3206
Ian T. Jacobsen <iantj92@gmail.com>
2014-04-13 10:19:56 +0100

    Setup and styling for header

--- public/js/zoidberg/zoidberg.js
+++ public/js/zoidberg/zoidberg.js
@@ -1,7 +1,14 @@
 angular.module("zoidberg", [])
 	.controller('HeaderCtrl', ['$scope', function($scope) {
 		$scope.items = [
-			{text: "Home", href: "/zoidberg/"},
-			{text: "Users", href: "/zoidberg/users"}
+			{text: "Home", href: "/zoidberg/", active: true},
+			{text: "Users", href: "/zoidberg/users", active: false}
 		];
+		$scope.ClickItem = function(item) {
+			for(var i = 0; i < $scope.items.length; i++)
+			{
+				$scope.items[i].active = false;
+			}
+			item.active = true;
+		}
 	}]);

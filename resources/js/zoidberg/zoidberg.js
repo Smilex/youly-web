3f22b8ab7f99b83b4c891ee2cc6128734dddaa37
Ian T. Jacobsen <iantj92@gmail.com>
2014-04-17 19:45:11 +0100

    Added breadcrumbs directive

--- resources/js/zoidberg/zoidberg.js
+++ resources/js/zoidberg/zoidberg.js
@@ -1,9 +1,9 @@
 angular.module("zoidberg", ["ui.router"])
 .config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise("/dash");
+	$urlRouterProvider.otherwise("/");
 	$stateProvider
-	.state("dash", {
-		url: "/dash",
+	.state("home", {
+		url: "/",
 		templateUrl: "dash.html"
 	});
 }])
@@ -42,4 +42,31 @@ angular.module("zoidberg", ["ui.router"])
 		link: link,
 		templateUrl: "notifications.html"
 	};
+}])
+.directive("zoidBreadcrumbs", ["$location", function ($location) {
+	function appendPath(element, path, icon) {
+		element.children().first().append("<li>" +
+			"<a href='#'>" +
+			(icon ? "<i class='" + icon + "'></i>" : "") +
+			path +
+			"</a></li>"
+			);
+	}
+	function link (scope, element, attrs) {
+		var paths = $location.path().split('/');
+		appendPath(element, "Home", "entypo-home");
+		for (var i = 0; i < paths.length; i++)
+		{
+			var path = paths[i];
+			if (!path) continue;
+			path = path.charAt(0).toUpperCase() + path.slice(1);
+			appendPath(element, path);
+		}
+		element.find("li").last().addClass("active");
+	}
+	return {
+		restrict: "E",
+		link: link,
+		template: "<ol class='breadcrumb bc-3'></ol>"
+	}
 }]);

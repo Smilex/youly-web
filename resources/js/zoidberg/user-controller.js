db03416c521ac26b7e578af91c12383da29cd04c
Ian T. Jacobsen <iantj92@gmail.com>
2014-04-17 23:43:39 +0100

    Added visual ques to read and unread notifications

--- resources/js/zoidberg/user-controller.js
+++ resources/js/zoidberg/user-controller.js
@@ -3,5 +3,27 @@ angular.module("zoidberg")
 	$scope.notifications = [];
 	$http.get("/zoidberg/users/notifications").success(function (data) {
 		$scope.notifications = data;
+		$scope.newNotifications = [];
+		$scope.GetNotifications = function () {
+			return $scope.notifications;
+		}
+		$scope.GetNewNotifications = function () {
+			if ($scope.newNotifications.length > 0)
+				return $scope.newNotifications;
+			var news = [];
+			for (var i = 0; i < $scope.notifications.length; i++)
+			{
+				if (!$scope.notifications[i].read)
+					news.push($scope.notifications[i]);
+			}
+			$scope.newNotifications = news;
+			return news
+		}
+		$scope.Read = function (notif) {
+			notif.read = true;
+			$scope.newNotifications = $scope.newNotifications.filter(function (element) {
+				return !element.read;
+			});
+		}
 	});
 }]);

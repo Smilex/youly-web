e310d145cbe1fe129f6b687c5ce0df6726b17fef
Ian T. Jacobsen <iantj92@gmail.com>
2014-06-26 14:25:53 +0100

    Added product search parse

--- resources/js/zoidberg/products-controller.js
+++ resources/js/zoidberg/products-controller.js
@@ -2,6 +2,7 @@ angular.module("zoidberg")
 .controller("ProductsCtrl", ["$scope","$http", "$state", function ($scope, $http, $state) {
 	$scope.p_edit = {};
 	$scope.products = [];
+	$scope.search_str = "";
 
 	$http.get("zoidberg/products", {})
 		.success(function (products) {
@@ -16,5 +17,31 @@ angular.module("zoidberg")
 	$scope.new = function () {
 		$scope.p_edit = {};
 		$state.go("products.edit");
+	}
+
+	$scope.parse_search = function () {
+		var str = $scope.search_str;
+		var spl = str.split(",");
+		var ret = [];
+		for (var i = 0; i < spl.length; i++) {
+			var s = spl[i].trim();
+			var l = s.split(":");
+			if (l.length < 2)
+				continue;
+
+			var id = l[0];
+			var val = l[1];
+
+			ret.push({id: id, val: val});
+		}
+
+		$http.post("zoidberg/products/search", ret)
+			.success(function (data) {
+				console.log("Success");
+				console.log(data);
+			})
+			.error(function () {
+				console.log("Error");
+			});
 	}
 }]);

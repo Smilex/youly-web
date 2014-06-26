76ae7456ebbdb4a32a66d8f3879d72b20d8500d1
Ian T. Jacobsen <iantj92@gmail.com>
2014-06-26 13:25:26 +0100

    Implemented products list logic

--- resources/js/zoidberg/products-controller.js
+++ resources/js/zoidberg/products-controller.js
@@ -1,6 +1,15 @@
 angular.module("zoidberg")
 .controller("ProductsCtrl", ["$scope","$http", "$state", function ($scope, $http, $state) {
 	$scope.p_edit = {};
+	$scope.products = [
+		{
+			barcode: "5741000138076",
+			name: "Booster Blue",
+			brand_name: "Faxe Kondi",
+			brand_owner: "Royal Unibrew",
+			category: "Drinks > Energy"
+		}
+	];
 
 	$scope.edit = function (p) {
 		$scope.p_edit = p;

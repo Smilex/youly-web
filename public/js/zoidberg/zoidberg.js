f4ee971abe6b81ad1e8b23bbb69e6903db1b7964
Ian T. Jacobsen <iantj92@gmail.com>
2014-07-10 16:36:50 +0100

    Added for merge

--- public/js/zoidberg/zoidberg.js
+++ public/js/zoidberg/zoidberg.js
@@ -1,1 +1,102 @@
-angular.module("zoidberg",["ui.router","ui.bootstrap"]).config(["$stateProvider","$urlRouterProvider",function(t,e){e.otherwise("/"),t.state("home",{url:"/",templateUrl:"dash.html"}).state("blogs",{url:"/blogs","abstract":!0,templateUrl:"blogs.html"}).state("blogs.list",{url:"/",templateUrl:"blog_list.html"}).state("blogs.edit",{url:"/edit",templateUrl:"blog_edit.html"}).state("products",{url:"/products","abstract":!0,templateUrl:"products.html"}).state("products.list",{url:"/",templateUrl:"products_list.html"}).state("products.edit",{url:"/edit",templateUrl:"products_edit.html"})}]).directive("zoidBreadcrumbs",["$location","$rootScope",function(t,e){function l(t,e,l){t.children().first().append("<li><a href='#'>"+(l?"<i class='"+l+"'></i>":"")+e+"</a></li>")}function r(e){e.children().html("");var r=t.path().split("/");l(e,"Home","entypo-home");for(var a=0;a<r.length;a++){var s=r[a];s&&(s=s.charAt(0).toUpperCase()+s.slice(1),l(e,s))}e.find("li").last().addClass("active")}function a(t,l){e.$on("$locationChangeSuccess",function(){r(l)})}return{restrict:"E",link:a,template:"<ol class='breadcrumb bc-3'></ol>"}}]).directive("zoidEditor",function(){var t={restrict:"E",replace:!0,transclude:!0,scope:{},template:'<div><textarea class="wysihtml5 form-control" rows="18"></textarea></div>',link:function(t,e,l){t.textarea=$(e.find("textarea")),t.wysi=t.textarea.wysihtml5({stylesheet:"/css/neon/wysihtml5-color.css",events:{change:function(){t.$parent.blog_edit.content=this.getValue(),t.$parent.save()}}}),t.wysi.val(l.value)}};return t});
+angular.module("zoidberg", ["ui.router", "ui.bootstrap"])
+.config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise("/");
+	$stateProvider
+	.state("home", {
+		url: "/",
+		templateUrl: "dash.html"
+	})
+	.state("blogs", {
+		url: "/blogs",
+		abstract: true,
+		templateUrl: "blogs.html"
+	})
+	.state("blogs.list", {
+		url: "/",
+		templateUrl: "blog_list.html"
+	})
+	.state("blogs.edit", {
+		url: "/edit",
+		templateUrl: "blog_edit.html"
+	})
+	.state("products", {
+		url: "/products",
+		abstract: true,
+		templateUrl: "products.html",
+		controller: "ProductsCtrl"
+	})
+	.state("products.list", {
+		url: "/",
+		templateUrl: "products_list.html"
+	})
+	.state("products.edit", {
+		url: "/edit",
+		templateUrl: "products_edit.html"
+	});
+}])
+.directive("zoidBreadcrumbs", ["$location", "$rootScope", function ($location, $rootScope) {
+	function appendPath(element, path, icon) {
+		element.children().first().append("<li>" +
+			"<a href='#'>" +
+			(icon ? "<i class='" + icon + "'></i>" : "") +
+			path +
+			"</a></li>"
+			);
+	}
+	function setPath(element) {
+		element.children().html("");
+		var paths = $location.path().split('/');
+		appendPath(element, "Home", "entypo-home");
+		for (var i = 0; i < paths.length; i++)
+		{
+			var path = paths[i];
+			if (!path) continue;
+			path = path.charAt(0).toUpperCase() + path.slice(1);
+			appendPath(element, path);
+		}
+		element.find("li").last().addClass("active");
+	}
+	function link (scope, element, attrs) {
+		$rootScope.$on("$locationChangeSuccess", function (e) {
+			setPath(element);
+		});
+	}
+	return {
+		restrict: "E",
+		link: link,
+		template: "<ol class='breadcrumb bc-3'></ol>"
+	}
+}])
+.directive("zoidEditor", function () {
+	var self = this;
+	var directive = {
+		restrict: "E",
+		replace: true,
+		transclude: true,
+		scope: {
+		},
+		template :
+		 
+			"<div>" +
+			 
+			"<textarea class=\"wysihtml5 form-control\" rows=\"18\"></textarea>"+
+			 
+			"</div>",
+		link: function ($scope, $element, $attrs) {
+			$scope.textarea = $($element.find("textarea"));
+			$scope.wysi = $scope.textarea.wysihtml5({
+				stylesheet: "/css/neon/wysihtml5-color.css",
+				events: {
+					"change": function () {
+						$scope.$parent.blog_edit.content = this.getValue();
+						$scope.$parent.save();
+					}
+				}
+			});
+
+			$scope.wysi.val($attrs.value);
+		}
+	};
+
+	return directive;
+});
